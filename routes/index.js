5b3994713039ad5785cdf6479691dac5e2a51030
nraghavan <nraghava@ucsd.edu>
2016-02-18 07:53:10 +0000

    Added Experiment Header

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,22 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
 exports.view = function(req, res){
+  	var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num >= 0) {
+  		projects['grid'] = false;
+  		res.render('index', projects);
+	} else {
+		projects['grid'] = true;
+  		res.redirect('/grid');
+	}
+};
+/*exports.view = function(req, res){
 	projects["grid"] = false;
   	res.render('index', projects);
-};
+};*/
 
 exports.viewGrid = function(req, res) {
 	projects["grid"] = true;
